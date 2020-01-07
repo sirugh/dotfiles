35a6bdcb268e173e82d825bc26ff834df3be4867
Stephen Rugh <rugh@adobe.com>
2020-01-07 14:08:01 -0600

    fix: nvm install and switcher
    Signed-off-by: Stephen Rugh <rugh@adobe.com>

--- nvm/completion.zsh
+++ nvm/completion.zsh
@@ -1,3 +1,7 @@
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm
+
 ## NVM autoswitch using .nvmrc
 # place this after nvm initialization!
 autoload -U add-zsh-hook
